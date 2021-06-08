51e386db1b01780f8e8b18d0c572d9b50739d9f3
root <rithymindiiit@gmail.com>
2021-06-08 14:50:31 +0530

    initail commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let app = require('express')();
 let http = require('http').Server(app);
-let io = require('socket.io')(http);
+let io = require('socket.io').listen(http);
+
  
 io.on('connection', function(socket) {
   console.log('A user connected');
