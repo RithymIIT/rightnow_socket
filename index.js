a0e91195bc9c16941bdf98139c447331a4840313
root <rithymindiiit@gmail.com>
2021-06-08 15:32:19 +0530

    add

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 let app = require('express');
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-
+const allowedOrigins = [
+   'capacitor://localhost',
+   'ionic://localhost',
+   'http://localhost',
+   'http://localhost:8080',
+   'http://localhost:8100'
+ ];
+const corsOptions = {
+   origin: (origin, callback) => {
+     if (allowedOrigins.includes(origin) || !origin) {
+       callback(null, true);
+     } else {
+       callback(new Error('Origin not allowed by CORS'));
+     }
+   }
+ }
  
+ // Enable preflight requests for all routes
+ app.options('*', cors(corsOptions));
+ 
+ app.get('/', cors(corsOptions), (req, res, next) => {
+   res.json({ message: 'This route is CORS-enabled for an allowed origin.' });
+ })
 io.on('connection', function(socket) {
   console.log('A user connected');
 
