40be531f3bd473ee14d8c80aaed0867d0cb5204d
root <rithymindiiit@gmail.com>
2021-06-09 21:10:26 +0530

    added

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ let io = require('socket.io')(http);
 io.origins('*:*')
 io.on('connection', function(socket) {
   console.log('A user connected');
-
+  socket.on('disconnect', function(){
+         io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+       });
+      
+       socket.on('set-nickname', (nickname) => {
+         socket.nickname = nickname;
+         io.emit('users-changed', {user: nickname, event: 'joined'});    
+       });
+       
+       socket.on('add-message', (message) => {
+         io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+       });
+     });
   //Send a message after a timeout of 4seconds
-  setTimeout(function() {
-     socket.send('Sent a message 4seconds after connection!');
-  }, 4000);
+//   setTimeout(function() {
+//      socket.send('Sent a message 4seconds after connection!');
+//   }, 4000);
 
-  socket.on('disconnect', function () {
-     console.log('A user disconnected');
-  });
-});
+//   socket.on('disconnect', function () {
+//      console.log('A user disconnected');
+//   });
+// });
 
 
 var port = process.env.PORT || 3001;
